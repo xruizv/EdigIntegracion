a1c8d9457627c05f167a28b09f3b29e014c25490
xruizv <xruizv@gmail.com>
2015-09-20 11:29:30 -0300

    add: first commit

---
+++ Integracion/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+using System.IO;
+using System.Xml.Linq;
+using System.Reflection;
+using ClosedXML.Excel;
+using System.Globalization;
+using System.Threading;
+
+namespace Integracion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Configurración de zona
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es-cl");
+            // Inicialización de variables
+            var appSettings = ConfigurationManager.AppSettings;
+            string rutaContribucionesF29 = appSettings.GetValues("contribucionesF29").First().ToString();
+            string rutaRemuneraciones = appSettings.GetValues("remuneraciones").First().ToString();
+            string rutaSalida = appSettings.GetValues("salida").First().ToString();
+            // Vemos que mes vamos a procesar, si no está en el app.config cargamos el mes actual
+            string anio = String.IsNullOrEmpty(appSettings.GetValues("anio").First().ToString()) ? DateTime.Now.Year.ToString("D2") : appSettings.GetValues("anio").First().ToString();
+            string mes = String.IsNullOrEmpty(appSettings.GetValues("mes").First().ToString()) ? DateTime.Now.Month.ToString("D4") : appSettings.GetValues("mes").First().ToString();
+            IList<Cobro> cobros = new List<Cobro>();
+            //Form29
+            string[] subdirectoryEntriesF29 = Directory.GetDirectories(rutaContribucionesF29);
+            string pathEmpresas = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ConfiguracionEmpresas.xml");
+            XElement configuracionEmpresas = XElement.Load(pathEmpresas);
+            IEnumerable<XElement> empresas = configuracionEmpresas.Descendants("empresa");
+            foreach (var item in subdirectoryEntriesF29)
+            {
+                string[] filesEntries = Directory.GetFiles(item);
+                string nombreArchivoMDBF29 = filesEntries.SingleOrDefault(x => x.Contains(String.Format("F29LGH{0}.MDB", anio.Substring(2, 2))));
+                string connectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}", nombreArchivoMDBF29);//User Id=admin;Password=;
+                string queryString = String.Format("SELECT d545 + d546 + d708 FROM Form29 WHERE Mes = {0}", int.Parse(mes).ToString());
+
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    double suma = 0;
+                    OleDbCommand command = new OleDbCommand(queryString, connection);
+                    try
+                    {
+                        connection.Open();
+                        OleDbDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            double numero = 0;
+                            double.TryParse(reader.GetValue(0).ToString(), out numero);
+                            suma += numero;
+                        }
+                        reader.Close();
+                        int rut;
+                        int.TryParse(Path.GetFileName(item), out rut);
+                        var empresa = empresas.SingleOrDefault(x => x.Element("rut").Value == rut.ToString());
+                        Rut bufferRut = new Rut(empresa.Element("rut").Value);
+                        Cobro cobro = new Cobro()
+                        {
+                            Empresa = empresa.Element("nombre").Value,
+                            Fecha = File.GetCreationTime(item),
+                            Moneda = "Pesos",
+                            Monto = suma,
+                            Producto = "Formulario 29",
+                            Origen = "Interno",
+                            RUT = bufferRut.Numero.ToString(),
+                            DV = bufferRut.DV.ToString(),
+                            NumeroDocumento = String.Empty
+                        };
+                        cobros.Add(cobro);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            // Remuneraciones formato Previred
+            string linea = "";
+            string[] subdirectoryEntriesRemuneraciones = Directory.GetDirectories(rutaRemuneraciones);
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ConfiguracionPrevired.xml");
+            XElement configuracionPrevired = XElement.Load(path);
+            IEnumerable<XElement> raiz = configuracionPrevired.Descendants("previred");
+            foreach (var item in subdirectoryEntriesRemuneraciones)
+            {
+                double acumulador = 0;
+                string[] filesEntries = Directory.GetFiles(item);
+                string nombreArchivoPrevired = filesEntries.SingleOrDefault(x => x.Contains(String.Format("PREVIRED{0}{1}", mes, anio)));
+                using (StreamReader sr = new StreamReader(nombreArchivoPrevired))
+                {
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        foreach (var subitem in raiz)
+                        {
+                            int largo = 0;
+                            int.TryParse(subitem.Element("largo").Value, out largo);
+                            int inicio = 0;
+                            int.TryParse(subitem.Element("inicio").Value, out inicio);
+                            int monto = 0;
+                            if (int.TryParse(linea.Substring(inicio, largo), out monto))
+                            {
+                                acumulador += monto;
+                            }
+                        }
+                    }
+                }
+                int identificador;
+                int.TryParse(Path.GetFileName(item).Replace("EMP",""), out identificador);
+                var empresa = empresas.SingleOrDefault(x => x.Element("identificador").Value == identificador.ToString());
+                Rut bufferRut = new Rut(empresa.Element("rut").Value);
+                Cobro cobro = new Cobro() {
+                    Empresa = empresa.Element("nombre").Value,
+                    Fecha = File.GetCreationTime(item),
+                    Moneda = "Pesos",
+                    Monto = acumulador,
+                    Producto = "Imposiciones",
+                    Origen = "Interno",
+                    RUT = bufferRut.Numero.ToString(),
+                    DV = bufferRut.DV.ToString(),
+                    NumeroDocumento = String.Empty
+                };
+                cobros.Add(cobro);
+            }
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Cobros");
+            IXLRow cabecera = worksheet.Row(1);
+            cabecera.Cell(1).SetValue("Empresa - Nombre de la empresa");
+            cabecera.Cell(2).SetValue("Rut ");
+            cabecera.Cell(3).SetValue("DV");
+            cabecera.Cell(4).SetValue("Origen");
+            cabecera.Cell(5).SetValue("Producto");
+            cabecera.Cell(6).SetValue("Fecha");
+            cabecera.Cell(7).SetValue("Monto");
+            cabecera.Cell(8).SetValue("Moneda - CLP");
+            cabecera.Cell(9).SetValue("Numero Documento");
+            cabecera.Cell(10).SetValue("Correlativo");
+            int i = 2;
+            foreach(var item in cobros)
+            {
+                IXLRow fila = worksheet.Row(i);
+                fila.Cell(1).SetValue(item.Empresa);
+                fila.Cell(2).SetValue(item.RUT);
+                fila.Cell(3).SetValue(item.DV);
+                fila.Cell(4).SetValue(item.Origen);
+                fila.Cell(5).SetValue(item.Producto);
+                fila.Cell(6).SetValue(item.Fecha.ToShortDateString());
+                fila.Cell(7).SetValue(item.Monto.ToString("C0"));
+                fila.Cell(8).SetValue(item.Moneda);
+                fila.Cell(9).SetValue(item.NumeroDocumento);
+                i++;
+            }
+            workbook.SaveAs(rutaSalida+"ArchivoSalida.xlsx");
+        }
+    }
+    public class Cobro
+    {
+        public string Empresa { get; set; }
+        public string RUT { get; set; }
+        public string DV { get; set; }
+        public string Origen { get; set; }
+        public string Producto { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+        public string Moneda { get; set; }
+        public string NumeroDocumento { get; set; }
+    }
+}
---
+++ Integracion/Rut.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Integracion
+{
+    public class Rut
+    {
+        public string RutCompleto
+        {
+            get
+            {
+                string rut = Numero.ToString();
+                string conPuntos = "";
+                int contador = 0;
+                string[] cadena = rut.Split();
+                for (int indice = rut.Length; indice > 0; indice--)
+                {
+                    conPuntos = rut.Substring(indice - 1, 1) + conPuntos;
+                    contador++;
+                    if (contador >= 3)
+                    {
+                        conPuntos = "." + conPuntos;
+                        contador = 0;
+                    }
+                }
+                return conPuntos + '-' + DV;
+            }
+        }
+        public int Numero { get; set; }
+        public char DV { get; set; }
+
+        public Rut() { }
+        public Rut(int numero)
+        {
+            Numero = numero;
+            DV = char.Parse(DigitoVerificador(numero));
+        }
+
+        public static Rut Parse(string s)
+        {
+            if (String.IsNullOrEmpty(s)) throw new ArgumentNullException("s");
+
+            Rut result = new Rut();
+            s = s.Trim().Replace("-", "");
+            s = s.Trim().Replace(".", "");
+            string rutTitularString = s.Substring(0, s.Length - 1);
+            result.Numero = int.Parse(rutTitularString);
+            result.DV = char.Parse(s.Substring(s.Length - 1).ToUpper());
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            if (!EsValido) return String.Empty;
+            return String.Format("{0}{1}", Numero, DV);
+        }
+
+        public string ToStringConGuion()
+        {
+            if (!EsValido) return String.Empty;
+            return String.Format("{0}-{1}", Numero.ToString("N0"), DV);
+        }
+
+        public static bool TryParse(string s, out Rut rut)
+        {
+            try
+            {
+                rut = Rut.Parse(s);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                rut = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="rutConDV">Rut en formato BSS (Sin puntos ni guion, con DV al final)</param>
+        public Rut(string rutConDV)
+        {
+            rutConDV = rutConDV.Trim();
+            rutConDV = rutConDV.Replace(".", "");
+            string rutTitularString = rutConDV.Substring(0, rutConDV.Length - 1);
+            try
+            {
+                Numero = int.Parse(rutTitularString);
+            }
+            catch (Exception e)
+            {
+                throw new System.FormatException(string.Format("El valor {0} no puede convertirse a rut", rutConDV), e);
+            }
+            DV = rutConDV.Substring(rutConDV.Length - 1).ToUpper().ToCharArray()[0];
+        }
+
+        /// <summary>
+        /// Revisa la validez del RUT utilizando el DV.
+        /// </summary>
+        public bool EsValido
+        {
+            get
+            {
+                return (VerificaRut(this.Numero, this.DV));
+            }
+        }
+
+        /// <summary>
+        /// Verifica que un rut sea valido
+        /// </summary>
+        /// <param name="Rut"></param>
+        /// <param name="DV"></param>
+        /// <returns></returns>
+        static public bool VerificaRut(int Rut, char DV)
+        {
+            // Chequeos que se hacen: 
+
+            if ((Rut.ToString().Length > 11) || (DV.ToString().ToUpper() != DigitoVerificador(Rut)))
+                return false;
+            else
+                return true;
+        }
+
+        /// <summary>
+        /// Calcula el digito verificador de un RUT
+        /// </summary>
+        /// <param name="rut"></param>
+        /// <returns>Digito verificador del rut</returns>
+        static public string DigitoVerificador(int rut)
+        {
+            int Digito;
+            int Contador;
+            int Multiplo;
+            int Acumulador;
+            string RutDigito;
+
+            Contador = 2;
+            Acumulador = 0;
+
+            while (rut != 0)
+            {
+                Multiplo = (rut % 10) * Contador;
+                Acumulador = Acumulador + Multiplo;
+                rut = rut / 10;
+                Contador = Contador + 1;
+                if (Contador == 8)
+                {
+                    Contador = 2;
+                }
+
+            }
+
+            Digito = 11 - (Acumulador % 11);
+            RutDigito = Digito.ToString().Trim();
+            if (Digito == 10)
+            {
+                RutDigito = "K";
+            }
+            if (Digito == 11)
+            {
+                RutDigito = "0";
+            }
+            return (RutDigito);
+        }
+    }
+}
